2fe284dd00aa4ef50152637b7d49cbcb0dceed7c
alex <alexyoung0119@gmail.com>
2026-01-16 16:15:05 -0600

    make c format default to filea or llvm

--- lua/cmds.lua
+++ lua/cmds.lua
@@ -38,6 +38,19 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = { 'c', 'cpp', 'h', 'hpp' },
+--   callback = function()
+--     vim.opt.tabstop = 4
+--     vim.opt.shiftwidth = 4
+--     vim.opt.softtabstop = 4
+--     vim.opt.expandtab = true
+--     vim.opt.smartindent = true
+--     vim.opt.autoindent = true
+--     vim.opt.smarttab = true
+--   end,
+-- })
+
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'java',
   callback = function()
